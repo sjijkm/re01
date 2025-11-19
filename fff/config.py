e856d725faec3cbf92ae6eb44cfd17971f2d4901
sjijkm <119107562+sjijkm@users.noreply.github.com>
2025-11-19 13:57:37 +0800

    Delete fff directory

--- fff/config.py
+++
@@ -1,9 +0,0 @@
-# 数据库配置
-DB_CONFIG = {
-    'host': 'localhost',
-    'database': 'greenhouse_system',
-    'user': 'root',  # 替换为你的MySQL用户名
-    'password': '123456',  # 替换为你的MySQL密码
-    'port': 3306,
-    'charset': 'utf8mb4'
-}
